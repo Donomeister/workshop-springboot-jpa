9a68af607e18578de51372a7c4e9602d89468cef
Luan <luan.donato10@gmail.com>
2024-10-09 13:22:51 -0300

    User entity and resource

---
+++ src/main/java/com/projetoteste/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.projetoteste.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetoteste.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Luan", "luan.donato10@gmail.com", "(17) 99739-6561", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
